0f2b61610d555c741563f5b16a47798751f42020
Enes Aky <enesakydev@gmail.com>
2025-05-25 18:09:52 +0300

    main-initial

---
+++ api/config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
+    "CONNECTİON_STRİNG": process.env.CONNECTİON_STRİNG || "mongodb://localhost27017/project_base"
+}
---
+++ api/routes/index.js
@@ -0,0 +1,10 @@
+var config = require('../config');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Enes' , config});
+});
+
+module.exports = router;
